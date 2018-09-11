c586c097fefc00c5c8f1da0e477c134b7eedd53a
Mathieu Lecarme <mlecarme@bearstech.com>
2018-09-11 18:33:46 +0200

    Listen

--- minasan.go
+++ minasan.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"os/signal"
 
 	"github.com/flashmob/go-guerrilla"
 	"gitlab.bearstech.com/factory/minasan/processor"
@@ -14,5 +16,12 @@ func main() {
 
 	if err == nil {
 		fmt.Println("Server Started!")
+	}
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	for sig := range c {
+		// sig is a ^C, handle it
+		fmt.Println(sig)
+		return
 	}
 }
