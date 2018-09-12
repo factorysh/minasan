ace82c8d597cf92e62c12dde57fbc4b28b5128d1
Mathieu Lecarme <mlecarme@bearstech.com>
2018-09-12 23:00:21 +0200

    Fix: Subject, not Title

--- debug_client.py
+++ debug_client.py
@@ -3,5 +3,8 @@ import smtplib
 
 server = smtplib.SMTP(host='localhost', port=2525)
 server.set_debuglevel(1)
-server.sendmail("debug@example.com", "group-projet@example.com", "This is fine")
+server.sendmail("debug@example.com", "factory-minasan@example.com", """From: robert
+Subject: super mail
+
+This is fine""")
 server.quit()
