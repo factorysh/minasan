7b3b7aa03c42f3be6bc00f21d2db33d97a82e4c0
Mathieu Lecarme <mlecarme@bearstech.com>
2018-09-18 14:02:39 +0200

    More Sentry

--- minasan.go
+++ minasan.go
@@ -1,16 +1,25 @@
 package main
 
 import (
+	"os"
+
 	"github.com/onrik/logrus/filename"
+	"github.com/onrik/logrus/sentry"
 	log "github.com/sirupsen/logrus"
+	"gitlab.bearstech.com/factory/minasan/cmd"
 )
 
-import "gitlab.bearstech.com/factory/minasan/cmd"
-
 func main() {
+	// logrus hook for sentry, if DSN is provided
+	dsn := os.Getenv("SENTRY_DSN")
+	if dsn != "" {
+		sentryHook := sentry.NewHook(dsn, log.PanicLevel, log.FatalLevel, log.ErrorLevel)
+		log.AddHook(sentryHook)
+	}
 	// Logrus hook for adding file name and line to logs
 	filenameHook := filename.NewHook()
-	log.SetLevel(log.InfoLevel)
 	log.AddHook(filenameHook)
+
+	log.SetLevel(log.InfoLevel)
 	cmd.Execute()
 }
