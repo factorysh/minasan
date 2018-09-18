eea81cda1182fd0a36318bfb308971ab37e528ba
Mathieu Lecarme <mlecarme@bearstech.com>
2018-09-18 11:54:45 +0200

    Config file

--- cmd/root.go
+++ cmd/root.go
@@ -3,7 +3,10 @@ package cmd
 import (
 	"fmt"
 	"os"
+	"path/filepath"
+	"strings"
 
+	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
@@ -11,6 +14,7 @@ import (
 var (
 	gitlabDomain       string
 	gitlabPrivateToken string
+	configFile         string
 )
 
 var rootCmd = &cobra.Command{
@@ -20,6 +24,25 @@ var rootCmd = &cobra.Command{
 
 func initConfig() {
 	viper.AutomaticEnv()
+	cfg := viper.GetString("config")
+	if cfg != "" {
+		abs, err := filepath.Abs(cfg)
+		if err != nil {
+			panic(err)
+		}
+		base := filepath.Base(abs)
+		path := filepath.Dir(abs)
+		viper.SetConfigName(strings.Split(base, ".")[0])
+		viper.AddConfigPath(path)
+		err = viper.ReadInConfig()
+		log.WithFields(log.Fields{
+			"path": path,
+			"base": base,
+		}).Info("Reading config file")
+		if err != nil {
+			panic(err)
+		}
+	}
 }
 
 func init() {
@@ -27,8 +50,10 @@ func init() {
 	pf := rootCmd.PersistentFlags()
 	pf.StringVarP(&gitlabDomain, "gitlab_domain", "g", "gitlab.example.com", "Gitlab domain")
 	pf.StringVarP(&gitlabPrivateToken, "gitlab_private_token", "t", "", "Gitlab private token")
+	pf.StringVarP(&configFile, "config", "c", "", "Config file")
 	viper.BindPFlag("gitlab_domain", rootCmd.PersistentFlags().Lookup("gitlab_domain"))
 	viper.BindPFlag("gitlab_private_token", rootCmd.PersistentFlags().Lookup("gitlab_private_token"))
+	viper.BindPFlag("config", rootCmd.PersistentFlags().Lookup("config"))
 }
 
 func Execute() {
