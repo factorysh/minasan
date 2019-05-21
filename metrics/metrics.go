9a0d95ae40f3235d420e3a88b9e8acb82900a0dc
nirae <hello@nicolasdubouilh.fr>
2019-05-21 15:12:33 +0200

    using promauto for remove init in metrics | fix and activate MailSentCounter and WrongProjectCounter

--- metrics/metrics.go
+++ metrics/metrics.go
@@ -5,28 +5,25 @@ import (
 	"net/http"
 
 	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	log "github.com/sirupsen/logrus"
 )
 
 var (
-	MailReceivedCounter = prometheus.NewCounter(prometheus.CounterOpts{
+	MailReceivedCounter = promauto.NewCounter(prometheus.CounterOpts{
 		Name: "mail_received",
 		Help: "Amount of mails receiveds",
 	})
-	MailSentCounter = prometheus.NewCounter(prometheus.CounterOpts{
+	MailSentCounter = promauto.NewCounter(prometheus.CounterOpts{
 		Name: "mail_sent",
 		Help: "Amount of mails sents",
 	})
-	WrongProjectCounter = prometheus.NewCounter(prometheus.CounterOpts{
-		Name: "Wrong project",
+	WrongProjectCounter = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "wrong_project",
 		Help: "Project that doesn't exist",
 	})
 )
-
-func init() {
-	prometheus.MustRegister(MailReceivedCounter)
-}
 
 func ListenAndServe(listen string) error {
 	http.Handle("/metrics", promhttp.Handler())
